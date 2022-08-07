f0d1338a224e90c1ed8ad64aeb40bcb2ef7fe66d
maciej <24421260+maciekole@users.noreply.github.com>
2022-08-07 22:38:15 +0200

    pdf report print

--- myshop/orders/urls.py
+++ myshop/orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views.order_view import order_create
+from .views.admin_view import admin_order_detail
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create/', order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', admin_order_detail, name='admin_order_detail'),
 ]
