29f7c97a30f67053a48e0684e7aff1168b968725
maciej <24421260+maciekole@users.noreply.github.com>
2022-08-14 01:31:09 +0200

    language and translation

--- myshop/payment/urls.py
+++ myshop/payment/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from payment.views.payment_view import payment_process, payment_done, payment_canceled
+from django.utils.translation import gettext_lazy as _
 
 app_name = 'payment'
 
 urlpatterns = [
-    path('process/', payment_process, name='process'),
-    path('done/', payment_done, name='done'),
-    path('canceled/', payment_canceled, name='canceled'),
+    path(_('process/'), payment_process, name='process'),
+    path(_('done/'), payment_done, name='done'),
+    path(_('canceled/'), payment_canceled, name='canceled'),
 ]
